e8e9cdf6130aae478740f45aaa82af9f0d58323c
Chris Newton <internet.intern1@gmail.com>
2024-03-29 09:51:30 -0400

    Player movement ideas

--- Rolldown/Assets/Scripts/CameraController.cs
+++ Rolldown/Assets/Scripts/CameraController.cs
@@ -6,11 +6,7 @@ using static UnityEngine.GraphicsBuffer;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    public float sensitivity = 3.0f;
     private Vector3 offset = new Vector3(0,6,-6);
-    private Vector3 localRot;
-    private float orbitDamping = 5f;
-    private float distanceToPlayer;
 
 
     // Start is called before the first frame update
@@ -23,23 +19,5 @@ public class CameraController : MonoBehaviour
     {
         // Follow player
         transform.position = player.transform.position + offset;
-
-        // Rotate camera around player
-        if (Input.GetKey(KeyCode.Mouse1))
-        {
-            CamOrbit();
-        }
-    }
-
-    private void CamOrbit() 
-    {
-        localRot.x += Input.GetAxis("Mouse X") * sensitivity;
-        localRot.y -= Input.GetAxis("Mouse Y") * sensitivity;
-
-        localRot.y = Mathf.Clamp(localRot.y, 15f, 60f);
-        localRot.x = Mathf.Clamp(localRot.x, -45f, 45f);
-
-        Quaternion QT = Quaternion.Euler(localRot.y, localRot.x, 0);
-        transform.rotation = Quaternion.Lerp(transform.rotation, QT, Time.deltaTime * orbitDamping);
     }
 }
--- Rolldown/Assets/Scripts/PlayerController.cs
+++ Rolldown/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float horizontalInput,verticalInput;
+    public float turnSpeed = 10000;
+    private Rigidbody rb;
+    private float currentX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(-turnSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(turnSpeed);
+        }
+
+    }
+
+    private void Move(float velocity)
+    {
+        currentX = transform.position.x + 1;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(currentX, transform.position.y, transform.position.z), velocity * Time.deltaTime);
     }
 }
