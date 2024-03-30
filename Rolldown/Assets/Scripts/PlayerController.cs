6cf4519ae81253e89ec70e086acb69fac64a49c5
Chris Newton <66325038+ChrisNewton51@users.noreply.github.com>
2024-03-30 10:52:52 -0400

    Movement updates

--- Rolldown/Assets/Scripts/PlayerController.cs
+++ Rolldown/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+    private Vector3 movement;
+    public float speed = 10f;
+    private bool onFloor = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Capture input from A and D keys
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        // Move the character using rigidbody.AddForce()
+        if (onFloor) MoveCharacter(movement);
+    }
+
+    void MoveCharacter(Vector3 direction)
+    {
+        // Apply force to the rigidbody
+        rb.AddForce(direction * speed);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(-Vector3.right * 5, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * 5, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor")) onFloor = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor")) onFloor = false;
     }
 }
