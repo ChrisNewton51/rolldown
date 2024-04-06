ddc969f1fca8c5fe57eb0b998ae02ea177ce7c64
Chris Newton <internet.intern1@gmail.com>
2024-04-06 19:15:43 -0400

    Movement tweaks. Being course design

--- Rolldown/Assets/Scripts/CameraController.cs
+++ Rolldown/Assets/Scripts/CameraController.cs
@@ -1,22 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0,6,-6);
+    public GameObject floor;
+    public float sensitivity = 80.0f;
+    public float distance = 8.0f;
+
+    private float orbitDamping = 10f;
+    private float x = 0.0f;
+    private float lookRange = 60f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     void LateUpdate()
     {
-        // Follow player
-        transform.position = player.transform.position + offset;
+        CamOrbit();
+    }
+
+    private void CamOrbit()
+    {
+        // Rotate camera around player
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            x += Input.GetAxis("Mouse X") * sensitivity * distance * 0.02f;
+            
+        }
+        x = Mathf.Clamp(x, -lookRange, lookRange);
+        Quaternion rotation = Quaternion.Euler(20, x, 0);
+        Vector3 position = rotation * new Vector3(0.0f, 3.5f, -distance) + player.transform.position;
+
+        transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * orbitDamping);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * orbitDamping);
     }
 }
--- Rolldown/Assets/Scripts/PlayerController.cs
+++ Rolldown/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float steadyForce = 2f;
+
     private Rigidbody rb;
-    private Vector3 movement;
-    public float speed = 10f;
+    private float horizontalInput, verticalInput;
     private bool onFloor = false;
+    private int appliedForce = 2;
+    private bool movingRight = false;
+    private int maxHorizSpeed = 15;
+    private bool jumping, negativeJump, sideJumping = false;
+    private float jumpTime = 0;
+    private float buttonTime = 0.2f;
+    private float jumpForce = 5;
+    private float gravityScale = 1.1f;
+    private bool onSideR, onSideL = false;
 
     void Start()
     {
@@ -17,38 +27,125 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Capture input from A and D keys
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        // Capture input from movement keys
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        Debug.Log(negativeJump);
+
+        MoveCharacter(horizontalInput, verticalInput);
     }
 
-    void FixedUpdate()
+    void MoveCharacter(float horizInput, float verticalInput)
     {
-        // Move the character using rigidbody.AddForce()
-        if (onFloor) MoveCharacter(movement);
-    }
+        // Movement while touching the floor of the course
+        
+        if (Mathf.Abs(rb.velocity.x) < maxHorizSpeed) rb.AddForce(new Vector3(horizInput, 0, 0) * steadyForce);
 
-    void MoveCharacter(Vector3 direction)
-    {
-        // Apply force to the rigidbody
-        rb.AddForce(direction * speed);
-
-        if (Input.GetKeyDown(KeyCode.A))
+        if (verticalInput < 0 && rb.velocity.z > 0)
         {
-            rb.AddForce(-Vector3.right * 5, ForceMode.Impulse);
+            rb.AddForce(new Vector3(0, 0, verticalInput) * steadyForce);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+
+        // Sharper turns while on floors
+        if (onFloor)
         {
-            rb.AddForce(Vector3.right * 5, ForceMode.Impulse);
+            if (Input.GetKeyDown(KeyCode.A) && movingRight)
+            {
+                rb.AddForce(new Vector3(-1, 0, 0) * appliedForce, ForceMode.Impulse);
+                movingRight = false;
+            }
+            if (Input.GetKeyDown(KeyCode.D) && !movingRight)
+            {
+                rb.AddForce(new Vector3(1, 0, 0) * appliedForce, ForceMode.Impulse);
+                movingRight = true;
+            }
+        }
+        
+        // On Sides
+        if (onSideR && horizInput < 0)
+        {
+            rb.AddForce(new Vector3(0, horizInput, horizInput * 0.1f) * steadyForce);
+            if (Input.GetKeyDown(KeyCode.A) && movingRight)
+            {
+                rb.AddForce(new Vector3(0, -1, -0.5f) * appliedForce, ForceMode.Impulse);
+                movingRight = false;
+            }
+
+        }
+        if (onSideL && horizInput > 0)
+        {
+            rb.AddForce(new Vector3(0, -horizInput, -horizInput * 0.1f) * steadyForce);
+            if (Input.GetKeyDown(KeyCode.D) && !movingRight)
+            {
+                rb.AddForce(new Vector3(0, -1, -0.5f) * appliedForce, ForceMode.Impulse);
+                movingRight = true;
+            }
+        }
+
+        // Jump handling
+        if (onFloor && Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+            negativeJump = true;
+            jumpTime = 0;
+        }
+
+        if ((onSideL || onSideR) && Input.GetKeyDown(KeyCode.Space))
+        {
+            sideJumping = true;
+            negativeJump = true;
+            jumpTime = 0;
+        }
+        if (jumping)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            jumpTime += Time.deltaTime;
+        }
+        if (sideJumping)
+        {
+            if (Mathf.Abs(transform.position.x) > 18)
+            {
+                rb.AddForce(new Vector3(0, 3 * Mathf.Abs(1 / transform.position.x), 0), ForceMode.Impulse);
+            } else
+            {
+                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            }
+            rb.AddForce(new Vector3(-transform.position.x * 0.002f, 0, 0), ForceMode.Impulse);
+            jumpTime += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) || jumpTime > buttonTime)
+        {
+            jumping = false;
+            sideJumping = false;
+        }
+        if (!onFloor && negativeJump) { 
+            rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass); 
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Floor")) onFloor = true;
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            onFloor = true;
+            negativeJump = false;
+        }
+        if (collision.gameObject.CompareTag("SideR"))
+        {
+            onSideR = true;
+            negativeJump = false;
+        }
+        if (collision.gameObject.CompareTag("SideL"))
+        {
+            onSideL = true;
+            negativeJump = false;
+        }
     }
 
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Floor")) onFloor = false;
+        if (collision.gameObject.CompareTag("SideR")) onSideR = false;
+        if (collision.gameObject.CompareTag("SideL")) onSideL = false;
     }
 }
