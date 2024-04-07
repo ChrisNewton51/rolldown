9db5fea7185f063a0ef33a3062918e553cedf17c
Chris Newton <internet.intern1@gmail.com>
2024-04-07 15:41:17 -0400

    More movement tweaks. More course development

--- Rolldown/Assets/Scripts/PlayerController.cs
+++ Rolldown/Assets/Scripts/PlayerController.cs
@@ -9,15 +9,15 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
     private float horizontalInput, verticalInput;
-    private bool onFloor = false;
+    private bool onFloor, onRamp = false;
     private int appliedForce = 2;
     private bool movingRight = false;
     private int maxHorizSpeed = 15;
     private bool jumping, negativeJump, sideJumping = false;
     private float jumpTime = 0;
-    private float buttonTime = 0.2f;
-    private float jumpForce = 5;
-    private float gravityScale = 1.1f;
+    private float buttonTime = 0.15f;
+    private float jumpForce = 0.22f;
+    private float gravityScale = 1.27f;
     private bool onSideR, onSideL = false;
 
     void Start()
@@ -30,60 +30,46 @@ public class PlayerController : MonoBehaviour
         // Capture input from movement keys
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        Debug.Log(negativeJump);
+        Debug.Log(rb.velocity.x);
 
         MoveCharacter(horizontalInput, verticalInput);
     }
 
     void MoveCharacter(float horizInput, float verticalInput)
     {
-        // Movement while touching the floor of the course
+        // Left and right movement
+        if (!onSideR && !onSideL)
+        {
+            LRMovement(horizInput, verticalInput);
+        }
         
-        if (Mathf.Abs(rb.velocity.x) < maxHorizSpeed) rb.AddForce(new Vector3(horizInput, 0, 0) * steadyForce);
+        //onSideR = (transform.position.x > 16.5) ? true : false;
+        //onSideL = (transform.position.x < -16.5) ? true : false;
 
+        // Always be able to slow down
         if (verticalInput < 0 && rb.velocity.z > 0)
         {
             rb.AddForce(new Vector3(0, 0, verticalInput) * steadyForce);
-        }
-
-        // Sharper turns while on floors
-        if (onFloor)
-        {
-            if (Input.GetKeyDown(KeyCode.A) && movingRight)
-            {
-                rb.AddForce(new Vector3(-1, 0, 0) * appliedForce, ForceMode.Impulse);
-                movingRight = false;
-            }
-            if (Input.GetKeyDown(KeyCode.D) && !movingRight)
-            {
-                rb.AddForce(new Vector3(1, 0, 0) * appliedForce, ForceMode.Impulse);
-                movingRight = true;
-            }
         }
         
         // On Sides
         if (onSideR && horizInput < 0)
         {
-            rb.AddForce(new Vector3(0, horizInput, horizInput * 0.1f) * steadyForce);
-            if (Input.GetKeyDown(KeyCode.A) && movingRight)
-            {
-                rb.AddForce(new Vector3(0, -1, -0.5f) * appliedForce, ForceMode.Impulse);
-                movingRight = false;
-            }
-
+            rb.AddForce(new Vector3(horizInput * 5 * Mathf.Abs(1 / transform.position.x), horizInput, 0) * steadyForce);
+        } else if (onSideR && horizInput > 0) 
+        {
+            LRMovement(horizInput, verticalInput);
         }
         if (onSideL && horizInput > 0)
         {
-            rb.AddForce(new Vector3(0, -horizInput, -horizInput * 0.1f) * steadyForce);
-            if (Input.GetKeyDown(KeyCode.D) && !movingRight)
-            {
-                rb.AddForce(new Vector3(0, -1, -0.5f) * appliedForce, ForceMode.Impulse);
-                movingRight = true;
-            }
+            rb.AddForce(new Vector3(horizInput * 5 * Mathf.Abs(1 / transform.position.x), -horizInput, 0) * steadyForce);
+        } else if (onSideL && horizInput < 0)
+        {
+            LRMovement(horizInput, verticalInput);
         }
 
         // Jump handling
-        if (onFloor && Input.GetKeyDown(KeyCode.Space))
+        if ((onFloor || onRamp) && Input.GetKeyDown(KeyCode.Space))
         {
             jumping = true;
             negativeJump = true;
@@ -98,19 +84,19 @@ public class PlayerController : MonoBehaviour
         }
         if (jumping)
         {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
             jumpTime += Time.deltaTime;
         }
         if (sideJumping)
         {
             if (Mathf.Abs(transform.position.x) > 18)
             {
-                rb.AddForce(new Vector3(0, 3 * Mathf.Abs(1 / transform.position.x), 0), ForceMode.Impulse);
+                rb.AddForce(new Vector3(0, 5 * Mathf.Abs(1 / transform.position.x), 0), ForceMode.Impulse);
             } else
             {
-                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+                rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
             }
-            rb.AddForce(new Vector3(-transform.position.x * 0.002f, 0, 0), ForceMode.Impulse);
+            rb.AddForce(new Vector3(-transform.position.x * 0.003f, 0, 0), ForceMode.Impulse);
             jumpTime += Time.deltaTime;
         }
         if (Input.GetKeyUp(KeyCode.Space) || jumpTime > buttonTime)
@@ -120,6 +106,22 @@ public class PlayerController : MonoBehaviour
         }
         if (!onFloor && negativeJump) { 
             rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass); 
+        }
+    }
+
+    void LRMovement(float horizInput, float verticalInput)
+    {
+        if (Mathf.Abs(rb.velocity.x) < maxHorizSpeed)
+        {
+            rb.AddForce(new Vector3(horizInput, 0, 0) * steadyForce);
+        }
+        else if (rb.velocity.x >= maxHorizSpeed && horizInput < 0)
+        {
+            rb.AddForce(new Vector3(horizInput, 0, 0) * steadyForce);
+        }
+        else if (-rb.velocity.x >= maxHorizSpeed && horizInput > 0)
+        {
+            rb.AddForce(new Vector3(horizInput, 0, 0) * steadyForce);
         }
     }
 
@@ -140,6 +142,11 @@ public class PlayerController : MonoBehaviour
             onSideL = true;
             negativeJump = false;
         }
+        if (collision.gameObject.CompareTag("Ramp"))
+        {
+            onRamp = true;
+            negativeJump = false;
+        }
     }
 
     private void OnCollisionExit(Collision collision)
@@ -147,5 +154,6 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Floor")) onFloor = false;
         if (collision.gameObject.CompareTag("SideR")) onSideR = false;
         if (collision.gameObject.CompareTag("SideL")) onSideL = false;
+        if (collision.gameObject.CompareTag("Ramp")) onRamp = false;
     }
 }
