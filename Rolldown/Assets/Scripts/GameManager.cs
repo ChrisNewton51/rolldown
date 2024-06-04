68dca404c40c5067f917d1caaa3ba2a6a6a6e589
Chris Newton <internet.intern1@gmail.com>
2024-06-03 21:05:35 -0400

    Add more UI

--- Rolldown/Assets/Scripts/GameManager.cs
+++ Rolldown/Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject titleScreen;
+    public GameObject pauseScreen;
 
     private GameObject[] players;
 
@@ -21,7 +22,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // Pause game
+        if (Input.GetKeyDown(KeyCode.Escape))
+            PauseGame();
     }
 
     public void StartGame()
@@ -31,5 +34,34 @@ public class GameManager : MonoBehaviour
             player.gameObject.SetActive(true);
         }
         titleScreen.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        pauseScreen.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        foreach (var player in players)
+        {
+            player.gameObject.transform.localPosition = new Vector3(0, -6.41f, -266.4171f);
+            player.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            player.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            player.gameObject.transform.rotation = Quaternion.identity;
+            player.gameObject.SetActive(false);
+        }
+        pauseScreen.SetActive(false);
+        titleScreen.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_STANDALONE
+                Application.Quit();
+        #endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
